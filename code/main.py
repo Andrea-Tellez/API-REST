0cbe802e4032b3bfe9c015f9b73fe00161cd473c
Andrea-Tellez <1719110195@utectulancingo.edu.mx>
2022-06-24 20:30:20 +0000

    commit

--- code/main.py
+++ code/main.py
@@ -6,6 +6,15 @@ from pydantic import BaseModel
 class Respuesta(BaseModel):
     message: str
 
+class Cliente(BaseModel):
+    id_cliente: int
+    nombre: str
+    email: str
+
+class ClienteIN(BaseModel):
+    nombre:str
+    email:str
+
 app=FastAPI()
 
 @app.get("/", response_model=Respuesta)
@@ -22,10 +31,48 @@ async def clientes():
         return response 
 
 @app.get("/clientes/{id}")
-async def clientes(id):
-    with sqlite3.connect("sql/clientes.sqlite") as connection:
+async def id_clientes(id):
+    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id))) 
         response = cursor.fetchall()
-        return response
+        return 
+
+"""segunda parte"""
+
+@app.post("/clientes/",response_model=Respuesta) 
+async def post_cliente(nombre:str,email:str): 
+    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO clientes(nombre,email) VALUES ('{}','{}')".format(nombre,email))
+        response = cursor.fetchone()
+        message = {"mensaje" : "Cliente agregado"}
+        return message
+        
+@app.put("/clientes/{id_cliente}",response_model=Respuesta)
+async def put_cliente(id_cliente:int,nombre:str,email:str):
+    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("UPDATE clientes SET nombre = '{}', email = '{}' WHERE id_cliente={}".format(nombre,email,id_cliente))
+        response = cursor.fetchone()
+        message = {"mensaje" : "Cliente actualizado"}
+        return message
+       
+@app.delete("/clientes/{id_cliente}",response_model=Respuesta)
+async def delete_cliente(id_cliente:int):
+   with sqlite3.connect("code/sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM clientes WHERE id_cliente={}".format(id_cliente))
+        response = cursor.fetchone()
+        message = {"mensaje" : "Cliente borrado"}
+        return message
+     
+
+
+
+
+
--- test/test_main.py
+++ test/test_main.py
@@ -15,3 +15,30 @@ def test_clientes():
     assert response.status_code == 200
     assert response.json() == [{"id_cliente":1,"nombre":"Dejah","email":"dejah@gmail.com"},
     {"id_cliente":2,"nombre":"John","email":"john@gmail.com"}, {"id_cliente":3,"nombre":"Carthoris","email":"carthoris@gmail.com"}]
+
+def test_id_clientes():
+    response = clientes.get('/clientes/1')
+    data = [{"id_cliente":1,"nombre":"Dejah","email":"dejah@gmail.com"}]
+    assert response.status_code == 200
+    assert response.json() == data
+
+def test_post_cliente():
+    payload = {"id_cliente":4"nombre":"Andrea", "email": "andrea@gmail.com"}
+    response = clientes.post("/clientes/", json=payload) 
+    Response = {"mensaje" : "Cliente agregado"}
+    assert response.status_code == 200
+    assert response.json() == Response
+
+def test_put_cliente():
+    payload = {"id_cliente":4"nombre" : "Monica", "email" : "monica@gmail.com"}
+    response = clientes.put("/clientes/",json=payload)
+    Response = {"mensaje" : "Cliente actualizado"}
+    assert response.status_code == 200
+    assert response.json() == Response
+
+def test_delete_cliente():
+    response = clientes.delete("/clientes/4")
+    Response = {"mensaje" : "Cliente borrado"}
+    assert response.status_code == 200
+    assert response.json() == Response
+
